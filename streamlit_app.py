1ccb95d723e5c13b624108a4cee94bbb80787bcb
geosirius <acordoba@pan-energy.com>
2024-10-09 16:12:26 -0300

    Update streamlit_app.py
    Cambio de app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,147 +1,204 @@
 import streamlit as st
+import lasio
+import zipfile
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors  # Importando el módulo colors desde matplotlib
-import seaborn as sns
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
-from sklearn.impute import SimpleImputer
-import zipfile
+from sklearn.preprocessing import MinMaxScaler
 import io
 
-# Título del Proyecto
-st.title("Clustering no supervisado en datos de registro de pozos")
+# Cargar el logo desde la URL
+st.image("https://energycouncil.com/wp-content/uploads/Pan-American-Energy.png", width=150)
+
+# Función para leer archivos LAS directamente
+def leer_archivo_las(las_file):
+    try:
+        las_bytes = las_file.read()  # Leer el archivo en formato de bytes
+        las = lasio.read(io.BytesIO(las_bytes))  # Convertir los bytes a formato BytesIO para que lasio pueda leerlo
+        df_las = las.df()
+        pozo_nombre = las.well.WELL.value
+        return pozo_nombre, df_las
+    except Exception as e:
+        st.error(f"Error al leer el archivo LAS: {e}")
+        return None, None
+
+# Función para leer archivos LAS desde un archivo ZIP
+def leer_archivos_las_zip(uploaded_file):
+    pozos_data = {}
+    try:
+        with zipfile.ZipFile(uploaded_file, "r") as z:
+            las_files = [name for name in z.namelist() if name.endswith(".las")]
+            st.write(f"Archivos .las encontrados: {las_files}")
+
+            if len(las_files) == 0:
+                st.error("El archivo ZIP no contiene archivos LAS.")
+                st.stop()
+
+            for las_filename in las_files:
+                try:
+                    with z.open(las_filename) as las_file:
+                        las_bytes = io.TextIOWrapper(las_file, encoding='utf-8')
+                        las = lasio.read(las_bytes)
+                        df_las = las.df()
+                        pozo_nombre = las.well.WELL.value
+                        pozos_data[pozo_nombre] = df_las
+                except Exception as e:
+                    st.error(f"Error al leer el archivo LAS {las_filename}: {e}")
+                    continue
+
+            st.success("Datos cargados con éxito!")
+    except zipfile.BadZipFile:
+        st.error("El archivo subido no es un ZIP válido.")
+    
+    return pozos_data
+
 
 # Descripción del proyecto
+st.markdown("<h2 style='text-align: center; color: #2E86C1;'>Clustering no supervisado en datos de registro de pozos</h2>", unsafe_allow_html=True)
+
 st.markdown("""
-### ¿Qué es esta aplicación?
+<style>
+body {
+    font-family: 'Arial', sans-serif;
+}
+</style>
+""", unsafe_allow_html=True)
 
-En geociencia y petrofísica, es esencial comprender la litología del subsuelo (es decir, las rocas y los sedimentos presentes) para una correcta interpretación de los datos. 
-Los registros de pozos (well logs) son mediciones tomadas dentro de un pozo perforado que nos brindan información sobre las propiedades físicas de las formaciones rocosas.
+st.write("En geociencia y petrofísica, es esencial comprender la litología del subsuelo para una correcta interpretación de los datos. "
+         "Los registros de pozos son mediciones que nos brindan información valiosa sobre las propiedades físicas de las formaciones rocosas a través de diversas curvas. "
+         "Esta aplicación realiza **clustering no supervisado** sobre datos de registro de pozos, con el objetivo de identificar patrones ocultos y agrupar los datos en base a similitudes.")
 
-Esta aplicación realiza **clustering no supervisado** sobre datos de registro de pozos para identificar patrones ocultos y agrupar los datos en base a similitudes. Utilizaremos dos métodos principales de clustering:
-- **K-Means**: Un algoritmo de clustering que divide los datos en un número específico de grupos (clusters) al minimizar la distancia entre los puntos y el centroide del cluster.
-- **Modelo de Mezcla Gaussiana (GMM)**: Un enfoque más flexible que permite modelar clusters de forma elíptica y calcular la probabilidad de que un punto pertenezca a un cluster.
+with st.expander("Curvas utilizadas en la interpretación"):
+    st.markdown("""
+    **GR (Gamma Ray)**: Mide la radiactividad natural de las formaciones rocosas. Ayuda a distinguir entre rocas que contienen arcillas (radiactivas) y rocas limpias como areniscas o calizas (no radiactivas). Un valor alto indica mayor contenido de arcilla.
 
-### ¿Cómo se manejan los datos faltantes?
+    **DTC (Sonic Travel Time)**: Tiempo de tránsito de una onda acústica a través de las formaciones. Se utiliza para calcular la porosidad y la litología. Valores más bajos suelen corresponder a formaciones densas y compactas, mientras que valores más altos indican porosidad o zonas de fractura.
 
-Es posible que algunos valores estén ausentes en los registros de pozos. Esta aplicación te permite manejar esos valores faltantes de dos formas:
-1. **Eliminar filas con datos faltantes**: Se eliminarán todas las filas con valores NaN.
-2. **Imputar datos faltantes**: Se reemplazarán los valores faltantes con la **mediana** de cada columna, lo que permite conservar más datos para el análisis.
+    **NPHI (Neutron Porosity)**: Estima la porosidad de la roca midiendo la respuesta a neutrones. Es útil para identificar zonas con contenido de gas, ya que el gas reduce la respuesta de porosidad.
 
-### ¿Qué se hará con los datos?
+    **RHOB (Density Porosity)**: Mide la densidad de la formación. Se usa para calcular la porosidad y evaluar la densidad de las rocas subterráneas. Un valor bajo sugiere una mayor porosidad, mientras que un valor alto indica una formación más densa y compacta.
 
-1. Subes un archivo comprimido (ZIP) que contiene el archivo CSV de los registros de pozos.
-2. La aplicación limpiará los datos y aplicará los algoritmos de clustering seleccionados.
-3. Se mostrarán gráficos para que visualices cómo se agrupan los datos de los registros de pozos según sus propiedades.
+    **PHI (Porosidad total)**: Indica el porcentaje de la roca que está compuesta de poros vacíos o llenos de fluidos (agua, gas, petróleo). Es fundamental para estimar el potencial de almacenamiento de hidrocarburos en la roca.
+    """)
 
----
-
-### Interpretación de los gráficos:
-
-- **Gráfico de registros de pozo (log plot)**: Este gráfico muestra varias curvas de propiedades (como densidad, porosidad y velocidad de las ondas acústicas) en función de la profundidad del pozo. Los diferentes colores representan los grupos o facies identificados por los algoritmos de clustering, así como las litologías conocidas (LIT_SI). Sirve para comparar visualmente los resultados del clustering con los datos reales de litología.
-
-- **LIT_SI (Simplificación de Litología)**: Es una simplificación de la columna de litología proporcionada. Se mapea la litología a números de 1 a 12 que representan diferentes tipos de rocas, como arenisca, esquisto, dolomita, etc. Esto facilita la comparación con los resultados de clustering.
-  - **KMeans**: Muestra los clusters generados por el algoritmo KMeans.
-  - **GMM**: Muestra los clusters generados por el modelo de mezcla gaussiana.
-
-- **Pairplot de Clustering**: El pairplot muestra las relaciones entre diferentes propiedades físicas (como el rayos gamma, la densidad, y la porosidad) y cómo se agrupan en diferentes clusters. Cada color representa un cluster diferente. Este gráfico es útil para entender cómo se distribuyen los datos y qué variables son más relevantes para la agrupación.
-
-¡Empecemos!
-""")
-
-# Cargar los datos
-uploaded_file = st.file_uploader("Sube un archivo ZIP que contenga el archivo CSV", type="zip")
-
-if uploaded_file is not None:
-    # Abrir el archivo ZIP y leer el archivo CSV
-    with zipfile.ZipFile(uploaded_file, 'r') as z:
-        # Obtener el nombre del archivo CSV dentro del archivo ZIP
-        file_name = z.namelist()[0]
-        
-        # Leer el CSV dentro del ZIP
-        with z.open(file_name) as f:
-            df = pd.read_csv(f)
+with st.expander("¿Cómo utilizar esta aplicación?"):
+    st.write("""
+    1. **Carga de archivos**: Los archivos de registro de pozos deben estar en formato `.las` y comprimidos en un archivo ZIP.
     
-    # Mostrar una descripción general del dataset
-    st.subheader("Descripción de los datos")
-    st.write(df.describe())
+    2. **Clustering no supervisado**: La aplicación permite aplicar diferentes algoritmos de clustering, como K-Means y Gaussian Mixture Model (GMM), para agrupar los datos en base a las propiedades de las formaciones rocosas.
 
-    # Filtrar las columnas necesarias
-    columnas = ["WELL", "DEPTH_MD", "RDEP", "RHOB", "GR", "NPHI", "PEF", "DTC", "FORCE_2020_LITHOFACIES_LITHOLOGY"]
-    workingdf = df[columnas].copy()
-    workingdf.rename(columns={'FORCE_2020_LITHOFACIES_LITHOLOGY': 'FACIES'}, inplace=True)
+    3. **Interpretación visual**: A partir del clustering, se generan gráficos que muestran las agrupaciones de las formaciones y cómo estas se correlacionan con las curvas de las propiedades físicas, facilitando la identificación de zonas con características geológicas similares.
+    """)
 
-    # Diccionario para interpretar la columna de litofacies
-    numeros_litologia = {30000: 'Arenisca', 65030: 'Arenisca/Esquisto', 65000: 'Esquisto', 80000: 'Marga',
-                         74000: 'Dolomita', 70000: 'Caliza', 70032: 'Tiza', 88000: 'Halita',
-                         86000: 'Anhidrita', 99000: 'Toba', 90000: 'Carbón', 93000: 'Basamento'}
-    numeros_litologia_simplificados = {30000: 1, 65030: 2, 65000: 3, 80000: 4, 74000: 5, 70000: 6, 70032: 7,
-                                       88000: 8, 86000: 9, 99000: 10, 90000: 11, 93000: 12}
+# Agrega más secciones o detalles según lo necesario
 
-    # Mapeo de la columna 'FACIES' con los nombres y números simplificados
-    workingdf['LIT'] = workingdf['FACIES'].map(numeros_litologia)
-    workingdf['LIT_SI'] = workingdf['FACIES'].map(numeros_litologia_simplificados)
+# Cargar archivo LAS o ZIP
+uploaded_file = st.file_uploader("Sube un archivo ZIP o un archivo LAS", type=["zip", "las"])
 
-    # Mostrar el dataframe filtrado
-    st.subheader("DataFrame filtrado")
-    st.write(workingdf.head())
+pozos_data = {}
 
-    # Sidebar para modificar las columnas que se usan
-    columnas_disponibles = workingdf.columns.tolist()
-    columnas_seleccionadas = st.sidebar.multiselect(
-        "Selecciona o quita columnas para el análisis", 
-        columnas_disponibles, 
-        default=columnas_disponibles
-    )
-    
-    if len(columnas_seleccionadas) > 0:
-        workingdf = workingdf[columnas_seleccionadas]
+if uploaded_file:
+    if uploaded_file.name.endswith(".zip"):
+        pozos_data = leer_archivos_las_zip(uploaded_file)
+    elif uploaded_file.name.endswith(".las"):
+        pozo_nombre, df_las = leer_archivo_las(uploaded_file)
+        if pozo_nombre:
+            pozos_data[pozo_nombre] = df_las
 
-        # Opción para manejar valores faltantes (NaN)
-        st.sidebar.header("Manejo de valores faltantes")
-        opcion_nan = st.sidebar.radio("¿Cómo deseas manejar los valores faltantes?", 
-                                      ("Eliminar filas con valores faltantes", "Imputar con la mediana"))
+# Parámetros de clustering y visualización
+if len(pozos_data) > 0:
+    st.subheader("Selección de Pozos")
 
-        if opcion_nan == "Eliminar filas con valores faltantes":
-            # Eliminar filas con NaN
-            workingdf = workingdf.dropna()
-        else:
-            # Imputar valores faltantes con la mediana
-            workingdf = workingdf.fillna(workingdf.median())
+    # Selección de pozos antes de realizar el clustering
+    pozo_nombres = list(pozos_data.keys())
+    pozos_seleccionados = st.multiselect("Selecciona los pozos a utilizar para el análisis", pozo_nombres, default=pozo_nombres)
 
-        # Seleccionar número de clusters
+    if len(pozos_seleccionados) > 0:
+        # Mostrar las columnas disponibles
+        columnas_disponibles = list(pozos_data[pozos_seleccionados[0]].columns)
+
+        # Validar si las columnas predeterminadas están en el dataset
+        curvas_predeterminadas = ["RES_DEEP", "GR", "RHOB", "NPHI"]
+        curvas_predeterminadas_validas = [col for col in curvas_predeterminadas if col in columnas_disponibles]
+
+        # Mostrar las curvas seleccionables con las predeterminadas que están en el dataset
+        curvas_seleccionadas = st.sidebar.multiselect("Selecciona las curvas para clustering", columnas_disponibles, default=curvas_predeterminadas_validas)
+
+        # Parámetros de Clustering
         st.sidebar.header("Parámetros de Clustering")
         n_clusters = st.sidebar.slider("Selecciona el número de clusters para KMeans y GMM", 2, 10, 5)
 
+        # Selección de pozo para graficar
+        pozo_seleccionado = st.sidebar.selectbox("Selecciona el pozo a graficar", pozos_seleccionados)
+
         # Clustering K-Means
-        kmeans = KMeans(n_clusters=n_clusters)
-        kmeans.fit(workingdf[['GR', 'RHOB', 'NPHI', 'DTC']])
-        workingdf['KMeans'] = kmeans.labels_
+        clustering_results = {}
 
-        # Clustering Gaussian Mixture
-        gmm = GaussianMixture(n_components=n_clusters)
-        gmm.fit(workingdf[['GR', 'RHOB', 'NPHI', 'DTC']])
-        etiquetas_gmm = gmm.predict(workingdf[['GR', 'RHOB', 'NPHI', 'DTC']])
-        workingdf['GMM'] = etiquetas_gmm
+        for pozo in pozos_seleccionados:
+            df = pozos_data[pozo]
 
-        # Visualización de los clusters
-        st.subheader("Resultados del clustering con KMeans y GMM")
-        st.write(workingdf[['WELL', 'DEPTH_MD', 'KMeans', 'GMM']].head())
+            # Filtrar las curvas seleccionadas para asegurar que existen en el DataFrame
+            curvas_validas = [curva for curva in curvas_seleccionadas if curva in df.columns]
 
-        # Función para graficar los resultados del clustering
+            # Verificar si hay curvas válidas disponibles
+            if len(curvas_validas) == 0:
+                st.warning(f"No se encontraron curvas válidas para el pozo {pozo}.")
+                continue
+
+            df_nonan = df[curvas_validas].dropna()
+
+            # Normalización de curvas (opcional)
+            normalizar = st.sidebar.checkbox("Normalizar curvas", value=False)
+            if normalizar:
+                scaler = MinMaxScaler()
+                df_nonan = pd.DataFrame(scaler.fit_transform(df_nonan), columns=curvas_validas, index=df_nonan.index)
+
+            # Clustering
+            kmeans = KMeans(n_clusters=n_clusters)
+            kmeans.fit(df_nonan)
+            df['KMeans'] = np.nan
+            df.loc[df_nonan.index, 'KMeans'] = kmeans.labels_
+
+            gmm = GaussianMixture(n_components=n_clusters)
+            etiquetas_gmm = gmm.fit_predict(df_nonan)
+            df['GMM'] = np.nan
+            df.loc[df_nonan.index, 'GMM'] = etiquetas_gmm
+
+            clustering_results[pozo] = df
+
+        # Selector de profundidades
+        st.sidebar.header("Selector de Profundidades")
+        profundidad_min = st.sidebar.number_input("Profundidad mínima", min_value=int(df.index.min()), max_value=int(df.index.max()), value=int(df.index.min()))
+        profundidad_max = st.sidebar.number_input("Profundidad máxima", min_value=int(df.index.min()), max_value=int(df.index.max()), value=int(df.index.max()))
+
+        # Filtrar los resultados por el rango de profundidad seleccionado
+        datos_filtrados = clustering_results[pozo_seleccionado][(clustering_results[pozo_seleccionado].index >= profundidad_min) & (clustering_results[pozo_seleccionado].index <= profundidad_max)]
+
+        # Visualización
+        st.subheader("Resultados del Clustering")
+
         def graficar_clusters(nombre_pozo, dataframe, curva_profundidad, curvas_a_graficar, curvas_facies=[]):
             colores_facies = ['#F4D03F', '#F5B041', '#DC7633', '#6E2C00', '#1B4F72', '#2E86C1', '#AED6F1', 
                               '#A569BD', '#196F3D', 'red', 'black', 'blue']
             num_tracks = len(curvas_a_graficar)
             
-            fig, ax = plt.subplots(nrows=1, ncols=num_tracks, figsize=(num_tracks*2, 10))
+            fig, ax = plt.subplots(nrows=1, ncols=num_tracks, figsize=(num_tracks * 2, 10))
             fig.suptitle(nombre_pozo, fontsize=20, y=1.05)
 
             for i, curva in enumerate(curvas_a_graficar):
                 if curva in curvas_facies:
-                    cmap_facies = colors.ListedColormap(colores_facies[0:dataframe[curva].max()], 'indexed')
+                    # Convertir el valor de max a entero y manejar casos NaN
+                    max_cluster = dataframe[curva].max()
+                    if pd.isna(max_cluster):
+                        max_cluster = 1
+                    else:
+                        max_cluster = int(max_cluster)
+                    
+                    cmap_facies = colors.ListedColormap(colores_facies[:max_cluster], 'indexed')
                     cluster = np.repeat(np.expand_dims(dataframe[curva].values, 1), 100, 1)
                     ax[i].imshow(cluster, interpolation='none', cmap=cmap_facies, aspect='auto',
                                  vmin=dataframe[curva].min(), vmax=dataframe[curva].max(),
@@ -160,25 +217,16 @@ if uploaded_file is not None:
             plt.tight_layout()
             return fig
 
-        # Gráficos de clustering por pozo
-        st.sidebar.subheader("Opciones de gráfico")
-        pozo_seleccionado = st.sidebar.selectbox("Selecciona el pozo a graficar", workingdf['WELL'].unique())
-        datos_pozo = workingdf[workingdf['WELL'] == pozo_seleccionado]
-        
-        fig = graficar_clusters(pozo_seleccionado, datos_pozo, datos_pozo['DEPTH_MD'], 
-                                ['GR', 'RHOB', 'NPHI', 'DTC', 'KMeans', 'GMM'], 
-                                curvas_facies=['KMeans', 'GMM', 'LIT_SI'])
+        # Graficar los resultados
+        fig = graficar_clusters(pozo_seleccionado, datos_filtrados, datos_filtrados.index, curvas_validas + ['KMeans', 'GMM'], curvas_facies=['KMeans', 'GMM'])
         st.pyplot(fig)
 
         # Pairplot para visualizar los clusters
         st.subheader("Pairplot para visualizar los clusters")
         metodo_clustering = st.selectbox("Selecciona el método de clustering", ['KMeans', 'GMM'])
         
-        fig_pairplot = sns.pairplot(datos_pozo, vars=['GR', 'RHOB', 'NPHI', 'DTC'], hue=metodo_clustering, palette='Dark2')
+        fig_pairplot = sns.pairplot(datos_filtrados, vars=curvas_validas, hue=metodo_clustering, palette='Dark2')
         st.pyplot(fig_pairplot)
 
-    else:
-        st.warning("Por favor, selecciona al menos una columna para el análisis.")
-
 else:
-    st.info("Sube un archivo ZIP para comenzar.")
+    st.info("Sube un archivo LAS o ZIP para comenzar.")
